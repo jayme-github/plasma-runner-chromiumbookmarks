5bc11a94e29b5b8260a01829c5f3b668cc0e8bd3
Jayme <tuxnet@gmail.com>
2012-08-14 09:21:27 +0200

    Remove code not related to bookmarks

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-plasma-runner-chromium
+plasma-runner-chromiumbookmarks
 ==============================
 
-Simple Krunner to lauch Chromium bookmarks and keywords (like Konqueror web shortcuts)
+Simple Krunner to lauch Chromium bookmarks
 
 Install
 -------
-    plasmapkg --type runner --install plasma-runner-chromium
+    plasmapkg --type runner --install plasma-runner-chromiumbookmarks
 
 Update
 ------
-    plasmapkg --type runner --upgrade plasma-runner-chromium
+    plasmapkg --type runner --upgrade plasma-runner-chromiumbookmarks
     kquitapp krunner
     krunner
--- contents/code/main.py
+++ contents/code/main.py
@@ -21,28 +21,19 @@ class ChromiumRunner(plasmascript.Runner):
 		tell the user how to use this runner
 		'''
 		logging.debug( 'ChromiumRunner init...' )
-		self._keywords = {}
 		self._bookmarks = []
 		
 		#FIXME: Should go to config
-		self._googleBaseURL = 'https://www.google.com/'
-		self._pathWebData = os.path.join( os.environ.get('HOME'), '.config/chromium/Default/Web Data' )
-		self._pathLocalState = os.path.join( os.environ.get('HOME'), '.config/chromium/Local State' )
 		self._pathBookmarks = os.path.join( os.environ.get('HOME'), '.config/chromium/Default/Bookmarks' )
 
-		self.setSyntaxes( [ Plasma.RunnerSyntax("<Chromium keyword> :q:", "Search for :q: using Chromium keyword"),
-							Plasma.RunnerSyntax(":q:", "Search for :q: in your Chromium bookmarks") ] )
+		self.setSyntaxes( [ Plasma.RunnerSyntax(":q:", "Search for :q: in your Chromium bookmarks") ] )
 
 		# Initially read data
-		self._readKeywords()
 		self._readBookmarks()
-		self._readLastKnownGoogleUrl()
 
 
 		# Watch the files for changes
 		self._watcher = KDirWatch(self)
-		self._watcher.addFile( self._pathWebData )
-		self._watcher.addFile( self._pathLocalState )
 		self._watcher.addFile( self._pathBookmarks )
 		self.connect(self._watcher, SIGNAL('created(QString)'), self.__updateData_created)
 		self.connect(self._watcher, SIGNAL('dirty(QString)'), self.__updateData_dirty)
@@ -59,33 +50,9 @@ class ChromiumRunner(plasmascript.Runner):
 		'''
 		Called by KDirWatch if a watched dir has changed (dirty)
 		'''
-		if path == self._pathWebData:
-			self._readKeywords()
-		elif path == self._pathLocalState:
-			# Gets triggered every 5 minutes... 
-			self._readLastKnownGoogleUrl()
-		elif path == self._pathBookmarks:
+		if path == self._pathBookmarks:
 			self._readBookmarks()
  
-	def _readKeywords(self):
-		'''
-		Copy Chromium Web Data as it is locked if Chromium running...
-		TODO: Is there a way to open sqlite db read-only if it is locked?
-		'''
-		if os.path.isfile( self._pathWebData ) and os.access( self._pathWebData, os.R_OK ):
-			fd, dbfile = mkstemp('krunner-chromium')
-			copy2( self._pathWebData, dbfile )
-			conn = sqlite3.connect( dbfile )
-			cur = conn.cursor()
-			cur.execute('SELECT `short_name`, `keyword`, `url` FROM keywords;')
-			self._keywords = {}
-			for row in cur.fetchall():
-				if not row[1] in self._keywords:
-					self._keywords[ row[1] ] = (row[0], row[2])
-			cur.close()
-			os.unlink( dbfile )
-			logging.debug( 'read %d unique keywords', len( self._keywords ) )
-
 	def _readBookmarks(self):
 		'''
 		Read Chromium bookmarks
@@ -110,84 +77,32 @@ class ChromiumRunner(plasmascript.Runner):
 					walk( bjson['roots'][key]['children'] )
 			logging.debug( 'read %d unique bookmarks', len( self._bookmarks ))
 
-	def _readLastKnownGoogleUrl(self):
-		'''
-		Read the last_known_google_url from "Local State"
-		'''
-		if os.path.isfile( self._pathLocalState ) and os.access( self._pathLocalState, os.R_OK ):
-			localStateFile = open( self._pathLocalState, 'r' )
-			localStateJson = json.load( localStateFile )
-			localStateFile.close()
-			if 'browser' in localStateJson and 'last_known_google_url' in localStateJson['browser']	and localStateJson['browser']['last_known_google_url']:
-				self._googleBaseURL = localStateJson['browser']['last_known_google_url']
-			logging.debug( 'read new googl url: "%s"', self._googleBaseURL )
-
 	def match(self, context):
 		'''
 		Called by krunner to let us add actions for the user
 		'''
-		if not context.isValid() or not self._keywords or not self._bookmarks:
+		if not context.isValid() or not self._bookmarks:
 			return
 		
-		# look for our keywords
-		for keyword in self._keywords:
-			if context.query().startsWith( keyword + ' ' ):
-				# ignore less than 3 characters (in addition to the keyword)
-				if len( context.query()[len(keyword):].trimmed() ) >= 3:
-					self._matchKeyword( context, keyword )
-
 		# look for bookmarks
 		def queryInBookmarks(element):
 			if context.query().toLower() in element['name'].lower():
 				return element
 		for match in filter( queryInBookmarks, self._bookmarks ):
-			self._matchBookmark( context, match )
+			# strip the keyword and spaces
+			q = context.query().trimmed()
 
-	def _matchBookmark(self, context, matchedBookmark):
-		# strip the keyword and spaces
-		q = context.query().trimmed()
-		
-		# Set location
-		self._location = matchedBookmark['url']
+			# Set location
+			self._location = matchedBookmark['url']
 
-		# create an action for the user, and send it to krunner
-		m = Plasma.QueryMatch(self.runner)
-		m.setText("%s: '%s'" % ( matchedBookmark['name'], self._location  ) )
-		m.setType(Plasma.QueryMatch.ExactMatch)
-		m.setIcon(KIcon("bookmarks")) 
-		m.setData( q )
-		context.addMatch(q, m)
+			# create an action for the user, and send it to krunner
+			m = Plasma.QueryMatch(self.runner)
+			m.setText("%s: '%s'" % ( matchedBookmark['name'], self._location  ) )
+			m.setType(Plasma.QueryMatch.ExactMatch)
+			m.setIcon(KIcon("bookmarks")) 
+			m.setData( q )
+			context.addMatch(q, m)
 
-	def _matchKeyword(self, context, matchedKeyword):
-		'''
-		Create QueryMatch instance for this keyword match
-		'''
-		# strip the keyword and spaces
-		q = context.query()[len(matchedKeyword):].trimmed()
-		
-		# TODO: Default google search
-		# Default google search URL is some freaky contruction like:
-		# {google:baseURL}search?{google:RLZ}{google:acceptedSuggestion}{google:originalQueryForSuggestion}{google:searchFieldtrialParameter}{google:instantFieldTrialGroupParameter}sourceid=chrome&client=ubuntu&channel=cs&ie={inputEncoding}&q=%s
-		# google:baseURL is in attr "last_known_google_url" in ~./config/chromium/Local State
-
-		# Resuling URL for query "fofo" is something like:
-		# https://www.google.de/search?aq=f&sourceid=chrome&client=ubuntu&channel=cs&ie=UTF-8&q=fofo
-		
-		if q:
-			self._location = self._keywords[matchedKeyword][1].replace('{searchTerms}', q)
-			# Quick workaround...
-			if self._location.startswith( '{google:baseURL}' ):
-				# Set "aq=f" if the user did not choose the query from the Google Suggest box.
-				self._location = urljoin( self._googleBaseURL, 'search?' + urlencode( {'q': q, 'aq': 'f'} ) )
-
-		# create an action for the user, and send it to krunner
-		m = Plasma.QueryMatch(self.runner)
-		m.setText("%s: '%s'" % (self._keywords[matchedKeyword][0], q) )
-		m.setType(Plasma.QueryMatch.ExactMatch)
-		m.setIcon(KIcon("chromium"))
-		m.setData(q)
-		context.addMatch(q, m)
- 
 	def run(self, context, match):
 		'''
 		called by KRunner when the user selects our action
